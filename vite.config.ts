382b9405f75600f7d255a3e9b6c62233ee9d2e73
Sam Stokes <stokes1003@gmail.com>
2024-06-22 18:16:45 -0500

    vite config updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config(); // Load environment variables from .env file
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_SERVICE_ID': JSON.stringify(
+      process.env.VITE_SERVICE_ID || ''
+    ),
+    'import.meta.env.VITE_TEMPLATE_ID': JSON.stringify(
+      process.env.VITE_TEMPLATE_ID || ''
+    ),
+    'import.meta.env.VITE_PUBLIC_KEY': JSON.stringify(
+      process.env.VITE_PUBLIC_KEY || ''
+    ),
+  },
+});
